89e6ee7cf716490337225c1ec96b0ea7f528ef15
Curiouspaul1 <170408025@live.unilag.edu.ng>
2020-10-02 02:18:49 +0100

    adjusted code to fit with PEP8 standards with the aid of flake8

--- audio_fourier_analysis.py
+++ audio_fourier_analysis.py
@@ -3,19 +3,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-fs = 2**12 # sample rate
-tp = 4 # sampling duration
-N = fs*tp # number of samples
+fs = 2**12  # sample rate
+tp = 4  # sampling duration
+N = fs*tp  # number of samples
+
 
 def fft(f):
     Ni = len(f)
     Mi = int(Ni / 2)
     if Mi <= 2:
-       return [f[0] + f[1] + f[2] + f[3], 
-               f[0] - 1j*f[1] - f[2] + 1j*f[3],
-               f[0] - f[1] + f[2] - f[3],
-               f[0] + 1j*f[1] - f[2] - 1j*f[3]]
-    
+        return [f[0] + f[1] + f[2] + f[3],
+                f[0] - 1j*f[1] - f[2] + 1j*f[3],
+                f[0] - f[1] + f[2] - f[3],
+                f[0] + 1j*f[1] - f[2] - 1j*f[3]]
+
     wn = math.cos(2*math.pi/Ni) - 1j*math.sin(2*math.pi/Ni)
     fe = [f[i] for i in range(Ni) if i % 2 == 0]
     fo = [f[i] for i in range(Ni) if i % 2 == 1]
@@ -23,15 +24,16 @@ def fft(f):
     Fo = fft(fo)
     return [np.around(Fe[i] + (wn**i)*Fo[i], decimals=10) for i in range(Mi)] + [np.around(Fe[i] - (wn**i)*Fo[i], decimals=10) for i in range(Mi)]
 
+
 recording = sd.rec(N, samplerate=fs, channels=1)
 print('Recording...')
 sd.wait()  # Wait until recording is finished
 print('Done!')
-x = [round(float(recording[i]), 10) for i in range(N)] # input sequence
+x = [round(float(recording[i]), 10) for i in range(N)]  # input sequence
 
-_X = fft(x) # discrete Fourier transform
-X = [round(Xi/N, 10) for Xi in _X] # frequency spectrum
-X_amp = [np.absolute(Xi) for Xi in X] # amplitude spectrum
+_X = fft(x)  # discrete Fourier transform
+X = [round(Xi/N, 10) for Xi in _X]  # frequency spectrum
+X_amp = [np.absolute(Xi) for Xi in X]  # amplitude spectrum
 
 M = int(N/2)
 ti = [i*tp/N for i in range(N)]
@@ -40,7 +42,7 @@ X_amp = np.array(X_amp[:M])*2
 
 plt.subplots_adjust(wspace=0.4, hspace=0.4)
 
-ax1= plt.subplot(211)
+ax1 = plt.subplot(211)
 plt.plot(ti, x)
 plt.xlabel('t (seconds)')
 plt.ylabel('x(t)')
--- digital_filter.py
+++ digital_filter.py
@@ -2,13 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-N = 2**12 # number of samples
-tp = 4 # sampling duration
+N = 2**12  # number of samples
+tp = 4  # sampling duration
+
 
 def signal(t): # signal to be filtered
     pi = math.pi
     sin = math.sin
     return 5*sin(2*pi*50*t) + 3*sin(2*pi*250*t) + 20*sin(2*pi*500*t)
+
 
 def digital_filter(x): # digital filter implementation
     y = []
@@ -20,6 +22,7 @@ def digital_filter(x): # digital filter implementation
         # second order Butterworth filter with cut-off frequency of 100Hz
         y.append(1.1429*y_1 - 0.4127*y_2 + 0.067*x[i] + 0.135*x_1 + 0.067*x_2)
     return y
+
 
 def fft(f):
     Ni = len(f)
@@ -37,6 +40,7 @@ def fft(f):
     Fo = fft(fo)
     return [np.around(Fe[i] + (wn**i)*Fo[i], decimals=10) for i in range(Mi)] + [np.around(Fe[i] - (wn**i)*Fo[i], decimals=10) for i in range(Mi)]
 
+
 def impulse_response(N):
     x = digital_filter([1 if i == 0 else 0 for i in range(N)])
     X = fft(x)
@@ -46,6 +50,7 @@ def impulse_response(N):
     fi = [i/tp for i in range(M)]
     X_amp = X_amp[:M]
     plot(1, fi, X_amp, 'f (hertz)', '|X(f)|', 'log', 'Impulse response')
+
 
 def plot(figure, x, y, xlabel, ylabel, xscale, title):
     plt.figure(figure)
@@ -57,6 +62,7 @@ def plot(figure, x, y, xlabel, ylabel, xscale, title):
     plt.minorticks_on()
     plt.grid(b=True, which='major', linestyle='-')
     plt.grid(b=True, which='minor', linestyle='--')
+
 
 impulse_response(N)
 
@@ -73,12 +79,18 @@ fi = [i/tp for i in range(M)]
 plot(3, fi, X_amp, 'f (hertz)', '|X(f)|', 'log', 'Signal in frequency domain')
 
 x_filtered = digital_filter(x)
-plot(4, ti, x_filtered, 't (seconds)', 'x(t)', 'linear', 'Filtered signal in time domain')
+plot(
+    4, ti, x_filtered,
+    't (seconds)', 'x(t)', 'linear', 'Filtered signal in time domain'
+    )
 
 _X_filtered = fft(x_filtered)
 X_filtered = [round(Xi/N, 10) for Xi in _X_filtered]
 X_amp_filtered = [np.absolute(Xi) for Xi in X_filtered]
 X_amp_filtered = np.array(X_amp_filtered[:M])*2
-plot(5, fi, X_amp_filtered, 'f (hertz)', '|X(f)|', 'log', 'Filtered signal in frequency domain')
+plot(
+    5, fi, X_amp_filtered,
+    'f (hertz)', '|X(f)|', 'log', 'Filtered signal in frequency domain'
+    )
 
 plt.show()
